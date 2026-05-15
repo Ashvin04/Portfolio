bf858b6e0e5a8e5d4d56985166a9b6e6925a724d
ash04 <ashvinjeevarasa20@gmail.com>
2026-05-15 18:04:05 +0100

    Configure GitHub Pages deployment with GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // This makes it work on GitHub Pages regardless of the repo name if using relative paths, or I can use /repo-name/
+  base: '/Portfolio/',
 })
